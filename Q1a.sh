c649fb2dd1d4161ce25fe49d07100124d79ffd44
HaniNguyen <haininh1803@gmail.com>
2019-05-26 22:54:20 -0700

    Fix Name

--- Q1a.sh
+++ Q1a.sh
@@ -2,5 +2,4 @@
 
 cut -d',' -f15,17 flightdelays.csv| grep "SFO"| head -n 3| cut -d',' -f1|csvlook > first3sfo.csv
 
-echo("Written by Hani")
-
+echo "Written by Hani"
--- Q1b.sh
+++ Q1b.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cut -d',' -f18 flightdelays.csv| sort| uniq -c |sort -r -n| head -n 3| csvlook
-echo("Written by Hani")
+
+echo "Written by Hani"
